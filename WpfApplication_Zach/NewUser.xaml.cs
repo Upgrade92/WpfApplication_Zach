89f022ab04b4d68259e444279f9867bdacc56851
Christian Thaler <c.thaler@szf.at>
2022-09-29 10:00:44 +0200

    Change to New User.cs -> Method: SaveButton_CLick

--- WpfApplication_Zach/NewUser.xaml.cs
+++ WpfApplication_Zach/NewUser.xaml.cs
@@ -14,7 +14,9 @@ namespace WpfApplication_Zach
 
         private void SaveButton_Click(object sender, RoutedEventArgs e)
         {
+            saveButton.Content = "Speichern";
             SaveNewUser();
+            
         }
 
         private void CloseButton_Click(object sender, RoutedEventArgs e)
